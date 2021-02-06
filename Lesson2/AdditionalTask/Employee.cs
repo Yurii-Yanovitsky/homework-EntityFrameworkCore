abc241be6af430c7e05bedc2c14ac1fa64e96470
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2021-02-06 03:15:07 +0200

    lesson-3: added additional task

---
+++ Lesson2/AdditionalTask/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace AdditionalTask
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Post { get; set; }
+        public IList<Customer> Customers { get; set; }
+    }
+}
---
+++ Lesson2/AdditionalTask/SampleContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace AdditionalTask
+{
+    public class SampleContextFactory : IDesignTimeDbContextFactory<ShopDBContext>
+    {
+        public ShopDBContext CreateDbContext(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonStream(File.OpenRead("appsettings.json"));
+
+            var config = builder.Build();
+
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ShopDBContext>();
+            var options = optionsBuilder
+                .UseSqlServer(connectionString)
+                .Options;
+
+            return new ShopDBContext(options);
+        }
+    }
+}
