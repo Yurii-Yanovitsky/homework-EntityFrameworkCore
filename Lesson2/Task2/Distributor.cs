05d6f94f80f268ff01b63dee74b913dec8e7136d
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2021-02-06 03:15:37 +0200

    lesson-3: added task2

---
+++ Lesson2/Task2/Distributor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Task2
+{
+    public class Distributor
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<Beer> Beers { get; set; } = new List<Beer>();
+    }
+}
