109bdc112bb8916289b5e6abdfea73f55eb12bcd
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2021-02-11 17:52:11 +0200

    lesson-5: added additional task

---
+++ Lesson5/AdditionalTask/Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace AdditionalTask
+{
+    public partial class Company
+    {
+        public Company()
+        {
+            Phones = new HashSet<Phone>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Phone> Phones { get; set; }
+    }
+}
