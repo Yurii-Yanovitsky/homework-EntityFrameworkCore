d40609e6727a5f68cd3b6ef7fb9b00b158535eb1
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2021-02-11 18:06:32 +0200

    lesson-5: added additional task

---
+++ Lesson5/AdditionalTask/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdditionalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var context = new PhoneAppDBContext();
+
+            Console.WriteLine("Information about all phones: ");
+
+            var phonesResult = context.Phones
+                .Include(p => p.Company)
+                .AsEnumerable()
+                .GroupBy(p => p.Company.Name)
+                .Select(g => new { Company = g.Key, Phones = g })
+                .ToList();
+
+            foreach (var group in phonesResult)
+            {
+                Console.WriteLine($"Company: {group.Company}");
+
+                foreach (var p in group.Phones)
+                {
+                    Console.WriteLine($"{p.Name} - {p.Price}");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.Write("Search: ");
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine();
+
+            var phones = context.Phones.Where(p => EF.Functions.Like(p.Name, $"%{input}%")).ToList();
+
+            foreach (var p in phones)
+            {
+                Console.WriteLine($"{p.Name} - {p.Price}");
+            }
+
+        }
+    }
+}
